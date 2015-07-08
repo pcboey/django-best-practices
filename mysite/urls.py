a1aaa2cd59d9625e8275ba2fcdd8d6adf5180d23
Boey Pak Cheong <pcboey@yahoo.com>
2015-07-08 23:44:03 +0800

    Added url for index.html and grid bootstrap example

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^grid/$', TemplateView.as_view(template_name='grid.html')),                   
     url(r'^books/', include('books.urls')),                   
     url(r'^admin/', include(admin.site.urls)),
 )
